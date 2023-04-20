2cadcece0a517654a3639b620b2bde9a724da229
Daniel Margolis <dargolis07@gmail.com>
2023-04-20 14:32:57 -0400

    Created Simulation
    Created initial simulation, grid and cell classes, and basic reproduction and infection logic.

--- src/cell.py
+++ src/cell.py
@@ -1,8 +1,85 @@
-
+from scipy.stats import bernoulli
 
 class BaseCell():
 
-    def __init__(self):
-        pass
+    def __init__(self, x, y, repro=.1, die=.05, infected=False):
+        self.repro_prob = repro #probability of Cell reproducing
+        self.die_prob = die #probability of Cell dying
+        self.x = x #x position in grid
+        self.y = y #y position in grid
+        self.infected = infected
+    
+    def reproduce(self, grid):
+        #reproduces cell if random number is less than reproduction probability
+        sample = bernoulli.rvs(self.repro_prob)
+        if sample == 1:
+            newCoords = grid.getEmptyNeighbor(self.x, self.y)
+            if newCoords:
+                newCell = BaseCell(newCoords[0], newCoords[1], self.repro_prob, self.die_prob, self.infected)
+                grid.add(newCoords[0], newCoords[1], newCell)
+
+    def infect(self, infection_prob, grid):
+        #infects neighbor cells if infected with probability of infection
+        if self.infected:
+            neighbors = grid.getNeighbors(self.x, self.y)
+            for neighbor in neighbors:
+                sample = bernoulli.rvs(infection_prob)
+                if sample == 1:
+                    grid.add(neighbor.x,neighbor.y,BaseCell(neighbor.x,neighbor.y,infected=True))
+    
+    def die(self, grid):
+        #kills cell if random number is less than death probability
+        sample = bernoulli.rvs(self.die_prob)
+        if sample == 1:
+            grid.add(self.x, self.y, None)
+    
+
+class Grid():
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = [[None for x in range(width)] for y in range(height)]
 
     
+    def add(self, x, y, cell):
+        self.grid[x][y] = cell
+
+    def get(self, x, y):
+        return self.grid[x][y]
+    
+    def getEmptyNeighbor(self, x,y):
+        #checks neighbors for an empty space around x and y coordinates
+        #returns coordinates of empty space or False if none are found
+        #if x is at max width or y at max height, does not check outside of range
+        if y+1 < self.height and self.grid[x][y+1] == None:
+            return (x, y+1)
+        elif y-1 >= 0 and self.grid[x][y-1] == None:
+            return (x, y-1)
+        elif x+1 < self.width and self.grid[x+1][y] == None:
+            return (x+1, y)
+        elif x-1 >= 0 and self.grid[x-1][y] == None:
+            return (x-1, y)
+        return False
+    
+    def getNeighbors(self, x,y):
+        #returns list of neighbor Cell objects
+        neighbors = []
+        if y+1 < self.height and self.grid[x][y+1] != None:
+            neighbors.append(self.grid[x][y+1])
+        if y-1 >= 0 and self.grid[x][y-1] != None:
+            neighbors.append(self.grid[x][y-1])
+        if x+1 < self.width and self.grid[x+1][y] != None:
+            neighbors.append(self.grid[x+1][y])
+        if x-1 >= 0 and self.grid[x-1][y] != None:
+            neighbors.append(self.grid[x-1][y])
+        return neighbors
+    
+    def getAllCells(self):
+        #returns list of all Cell objects in grid
+        cells = []
+        for row in self.grid:
+            for cell in row:
+                if cell != None:
+                    cells.append(cell)
+        return cells
--- src/main.py
+++ src/main.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python3
+from cell import BaseCell, Grid
+import random as rand
 
+def start(infection_prob, repro_prob, die_prob,steps=100, width=100, height=100, numCells=200, numInfected=20):
+    #creates grid and adds cells randomly to grid and randomly infects some of them
+    grid = Grid(width, height)
+    for i in range(numCells):
+        x = rand.randint(0, width-1)
+        y = rand.randint(0, height-1)
+        if grid.get(x,y) == None:
+            cell = BaseCell(x, y, repro_prob, die_prob, False)
+            grid.add(x, y, cell)
+        else:
+            i -= 1
+    infected = 0
+    while infected < numInfected:
+        x = rand.randint(0, width-1)
+        y = rand.randint(0, height-1)
+        cell = grid.get(x,y)
+        if cell != None and not cell.infected:
+            cell.infected = True
+            infected += 1
+    print("Initial Conditions:" + "\n" + "Number of living Cells: " + str(numCells) + "\n" + "Number of infected Cells: " + str(numInfected))
+    print("Grid size: " + str(width) + "x" + str(height) + "\n")
+    step = 1
+    while step <= steps:
+        cells = grid.getAllCells()
+        print("Epoch: " + str(step))
+        print("Number of living Cells: " + str(len(cells)))
+        print("Number of infected Cells: " + str(len([cell for cell in cells if cell.infected])))
+        
+        for cell in cells:
+            cell.reproduce(grid)
+            cell.infect(infection_prob, grid)
+            cell.die(grid)
+        step += 1
+    
 def main():
-
-    pass
-
+    start(0.2, .5, .2)
+    
 main()
